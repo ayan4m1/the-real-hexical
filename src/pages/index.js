cfe618259cff2d4ae00d4ee8782f8048e8b94d90
ayan4m1 <andrew@bulletlogic.com>
2024-06-07 08:07:24 -0400

    get comp in the mix (fuck you comp)

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,20 @@ import paypalProof from '../images/proof-paypal.png';
 import firstDiscordProof from '../images/proof-discord.png';
 import secondDiscordProof from '../images/proof-discord-2.png';
 import thirdDiscordProof from '../images/proof-discord-3.png';
+import firstERProof from '../images/proof-er.png';
+import secondERProof from '../images/proof-er-2.png';
+import thirdERProof from '../images/proof-er-3.png';
 
 import Layout from '../components/layout.js';
 import ContentCard from '../components/contentCard.js';
 
 export default function IndexPage() {
   return (
-    <Layout title="Home" description="The truth about ItsHexical.">
+    <Layout title="The Real Hexical" description="ItsHexical is a scammer!">
       <Container>
         <ContentCard
           icon={faExclamationTriangle}
-          text="ItsHexical (hexical.me) Is A Fraud"
+          text="ItsHexical (hexical.me) Is A Scammer"
         >
           <Container fluid>
             <Row className="d-flex align-items-xl-center">
@@ -49,12 +52,11 @@ export default function IndexPage() {
                   server.
                 </p>
                 <p>
-                  This all changed when he stole a couple hundred dollars
-                  outright by taking a keyboard commission from our mutual
-                  friend, then ghosted him and me at the same time once it
-                  became clear he was not going to ship a keyboard. He was
-                  banned from the DIY eJuice Discord server as a result of this
-                  conduct.
+                  This all changed when he stole a couple hundred dollars by
+                  taking a keyboard commission from our mutual friend, then
+                  ghosting him and me at the same time once it became clear he
+                  was not going to ship a keyboard. He was banned from the DIY
+                  eJuice Discord server as a result of this conduct.
                 </p>
                 <p>
                   He claims that he was unable to ship the keyboard because of a
@@ -66,9 +68,14 @@ export default function IndexPage() {
                 </p>
                 <p>
                   Hex stopped responding to my messages and emails, then banned
-                  me from all mutual Discord servers he had that power on. Hex
-                  owed me about $1,200 in unpaid loans at the time. I reported
-                  these concerns to the admins of the{' '}
+                  me from all mutual Discord servers he had that power on. It is
+                  obvious from those actions that he knew he was responsible for
+                  the keyboard, but chose to try and avoid an uncomfortable
+                  situation instead of being honest and upfront.
+                </p>
+                <p>
+                  Also, Hex owed me about $1,200 in unpaid loans at the time. I
+                  reported these concerns to the admins of the{' '}
                   <a
                     href="https://www.escaperestart.com"
                     target="_blank"
@@ -76,10 +83,13 @@ export default function IndexPage() {
                   >
                     EscapeRestart
                   </a>{' '}
-                  gaming community, who tried to sweep it under the table
-                  because Hex was in the process of stepping down as their lead
-                  community admin. I had to call him out on their Discord server
-                  because he has ghosted me everywhere else.
+                  gaming community, who accepted the validity of my assertions
+                  but then tried to sweep them under the table because Hex was
+                  in the process of stepping down as their lead community admin.
+                  I had to call him out on their Discord server because he had
+                  ghosted me everywhere else. Now he will not return my emails
+                  again, because he is a child who runs from difficult
+                  conversations.
                 </p>
                 <h3>Moving Forward</h3>
                 <p>
@@ -129,6 +139,34 @@ export default function IndexPage() {
                   <Figure.Image width={480} src={pirateshipProof} />
                   <Figure.Caption className="text-center">
                     PirateShip Transaction List Showing Packages Sent To Hex
+                  </Figure.Caption>
+                </Figure>
+              </Col>
+            </Row>
+            <Row className="d-flex justify-content-center align-items-center">
+              <Col xs={4}>
+                <Figure>
+                  <Figure.Image width={480} src={firstERProof} />
+                  <Figure.Caption className="text-center">
+                    Showing that EscapeRestart admin believed my evidence
+                  </Figure.Caption>
+                </Figure>
+              </Col>
+              <Col xs={4}>
+                <Figure>
+                  <Figure.Image width={480} src={secondERProof} />
+                  <Figure.Caption className="text-center">
+                    Showing that EscapeRestart admin said Hex needed to step
+                    down in part because of his actions
+                  </Figure.Caption>
+                </Figure>
+              </Col>
+              <Col xs={4}>
+                <Figure>
+                  <Figure.Image width={480} src={thirdERProof} />
+                  <Figure.Caption className="text-center">
+                    Showing that the same admin then told me not to dicusss this
+                    when I had no other venue to discuss it
                   </Figure.Caption>
                 </Figure>
               </Col>
